d357b4715a38575ad844d265d099eae47c3bea27
Syver <syverdklungsoyr@lewisu.edu>
2024-02-25 22:30:12 -0600

    Ait

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+
 var http = require('http');
 var url = require('url');
 var dt = require('./datetime');
 
 
+
+
 const server = http.createServer((request, response) => {
     // Write the request to the log. 
     console.log(request);
-
+    
     // Standard Hello World.
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('<h3>Hello World!</h3>')
@@ -23,9 +32,30 @@ const server = http.createServer((request, response) => {
     var txt = q.year + " " + q.month;
     response.write("txt="+txt);
 
+    
     // Close the response
     response.end('<h3>The End.</h3>');
 });
+
+function RollIndividualDice() {
+    return Math.floor(Math.random()*6) + 1;
+}
+
+app.get('/roll', (req, res) => {
+    const result = Math.floor(Math.random() * 6) + 1; 
+    res.json({ result });
+});
+
+res.json({ results });
+
+
+function rollDice() {   
+    document.getElementById("dice1").value = RollIndividualDice();
+    document.getElementById("dice2").value = RollIndividualDice();
+    document.getElementById("dice3").value = RollIndividualDice();
+    document.getElementById("dice4").value = RollIndividualDice();
+    document.getElementById("dice5").value = RollIndividualDice();
+}
 
 const port = process.env.PORT || 1337;
 server.listen(port);
